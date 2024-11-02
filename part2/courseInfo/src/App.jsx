43ed0c8b1012235d8ec3a0f2c00e1bdd662c0595
smoky <smoky@smoky.>
2024-11-02 23:44:16 +0530

    declare seprate component for course

--- part2/courseInfo/src/App.jsx
+++ part2/courseInfo/src/App.jsx
@@ -1,3 +1,5 @@
+import CourseList from "./Course";
+
 const App = () => {
   const courses = [
     {
@@ -27,43 +29,6 @@ const App = () => {
   );
 };
 
-const CourseList = ({ courses }) => {
-  return (
-    <div>
-      <h1>Web Development Curriculum</h1>
-      {courses.map(course => (
-        <Course key={course.id} course={course} />
-      ))}
-    </div>
-  );
-};
 
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <strong>Total of {totalExercises} exercises</strong>
-    </div>
-  );
-};
-
-const Header = ({ name }) => <h3>{name}</h3>;
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => (
-      <Part key={part.id} name={part.name} exercises={part.exercises} />
-    ))}
-  </div>
-);
-
-const Part = ({ name, exercises }) => (
-  <p>
-    {name} {exercises}
-  </p>
-);
 
 export default App;
